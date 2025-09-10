e399a601ee4916ececd7545ffc96b993667c281e
Kay11 <kabelowrty2@gmail.com>
2025-09-10 16:30:31 +0200

    up

--- JS/script.js
+++ JS/script.js
@@ -249,6 +249,11 @@ if (formEl && window.location.pathname.includes("reset.html")) {
     window.location.href = "../PAGES/addBusiness.html";
   });
 
+  document.querySelector(".btn-add")?.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "../PAGES/registerBusiness.html";
+  });
+
   document.querySelector(".btn-list")?.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "../PAGES/mybusiness.html";
