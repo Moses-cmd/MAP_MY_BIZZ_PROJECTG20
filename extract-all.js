b89e6d2b3a8ead257fc894cfb86af01d98feb49d
Kay11 <kabelowrty2@gmail.com>
2025-09-10 11:01:09 +0200

    updates version

--- extract-all.js
+++ extract-all.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import fs from 'fs'
 
-const supabaseUrl="https://baojrbvrciawwqkyflxp.supabase.co"
-const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhb2pyYnZyY2lhd3dxa3lmbHhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMDc4NjEsImV4cCI6MjA3MTc4Mzg2MX0.h7a0fechNuunOyJH4tckcA6Dc47yoFvAFaS18LvQCiQ"
+const SUPABASE_URL = 'https://your-project-id.supabase.co';
+const SUPABASE_SERVICE_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhb2pyYnZyY2lhd3dxa3lmbHhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMDc4NjEsImV4cCI6MjA3MTc4Mzg2MX0.h7a0fechNuunOyJH4tckcA6Dc47yoFvAFaS18LvQCiQ";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
